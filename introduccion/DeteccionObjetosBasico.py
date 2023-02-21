d46b64a280729168ec1b6ed53eb55475cdccc6e2
roviamer <roviamer@gmail.com>
2023-02-21 14:47:24 -0600

    Clasificador con descriptor pixel

---
+++ introduccion/DeteccionObjetosBasico.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+@author: JaimesPita
+"""
+
+
+
+import cv2
+import numpy as np
+
+class DeteccionObjetosBasico:
+    def __init__(self,imagen):
+        self.imagen=imagen
+        self.x=imagen.shape[0]
+        self.y=imagen.shape[1]       
+    
+    def clasificador_pixel(self):  #Realiza una clasificación  utilizando el descriptor del pixel   
+        self.imagen_bn=np.zeros_like(self.imagen,dtype=np.uint8)              
+        for x in range(imagen.shape[0]):
+            for y in range(imagen.shape[1]):
+                if ((fronteras['Bmin']<imagen[x,y,0]<fronteras['Bmax'])  and (fronteras['Gmin']<imagen[x,y,1]<fronteras['Gmax']) and (fronteras['Rmin']<imagen[x,y,2]<fronteras['Rmax'])):
+                    self.imagen_bn[x,y,:]=255
+        self.mostrar_imagen()
+                                    
+    def mostrar_imagen(self):
+        #imagen_bn=np.expand_dims(self.imagen_bn,axis=2)
+        imagen=cv2.hconcat([self.imagen,self.imagen_bn])
+        
+        cv2.imshow('img',imagen)
+        cv2.waitKey(0)
+        
+imagen=cv2.imread(".\imagenes\hormigas.jpg",1)
+fronteras={'Bmin':50,'Bmax':90,'Gmin':50,'Gmax':90,'Rmin':100,'Rmax':145}
+deteccion1=DeteccionObjetosBasico(imagen)
+deteccion1.clasificador_pixel()
+#deteccion1.mostrar_imagen()
+
---
+++ introduccion/pixel_descriptor.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 21 12:08:10 2023
+
+@author: st
+"""
+
+import cv2
+
+import cv2
+import numpy as np
+
+imagen=cv2.imread(".\imagenes\hormigas.jpg",1)
+imagen_bn=np.zeros((imagen.shape[0],imagen.shape[1]))
+
+
+               
+for x in range(imagen.shape[0]):
+    for y in range(imagen.shape[1]):
+        if ((50<imagen[x,y,0]<90)  and (50<imagen[x,y,1]<90) and (100<imagen[x,y,2]<145)):
+            imagen_bn[x,y]=1;
+           
+cv2.imshow('img',imagen_bn)
+cv2.waitKey(0)
